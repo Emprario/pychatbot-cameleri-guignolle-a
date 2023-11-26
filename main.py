d52b31e77dddb47961a1b2722d0c6c4462551a04
Balafre78 <balafre.78@outlook.fr>
2023-11-26 18:42:20 +0100

    Ajout fonction mot le plus répété président

--- main.py
+++ main.py
@@ -55,7 +55,31 @@ def max_tfidf_words() -> None:
     print_list("Mot(s) ayant le score TF-IDF le plus élevé :", list_words)  # Affichage
 
 
-def f3(): pass
+def max_repeated(president: str) -> None:
+    """
+    Affiche le(s) mot(s) le(s) plus répété(s) par le président fourni
+    :param president: le président à étudier
+    """
+    infile = ""  # Création d'un grand texte à partir des discours du président
+    for file in extract_names(CORPUS_CLEAN)[president]:
+        target = open(CORPUS_CLEAN + file, 'r', encoding="utf-8")
+        infile += target.read() + " "
+        target.close()
+    tf = tf_occurences(infile)  # Calcul d'occurrence des mots dans tous les discours du président
+
+    # Recherche du(des) mot(s) le plus occurrences
+    occ_max = 0
+    list_words = []
+    for word in tf:
+        if tf[word] > occ_max:
+            occ_max = tf[word]
+            list_words = [word]
+        elif tf[word] == occ_max:
+            list_words.append(word)
+
+    print_list("Mot(s) le(s) plus répété(s) par le président Chirac :", list_words)  # Affichage
+
+
 def f4(): pass
 def f5(): pass
 def f6(): pass
@@ -68,7 +92,7 @@ OPTIONS = (
     ("Sortir", exit),
     ("Afficher la liste des mots les moins importants", less_important_words),
     ("Afficher le(s) mot(s) ayant le score TF-IDF le plus élevé", max_tfidf_words),
-    ("Afficher le(s) mot(s) le(s) plus répété(s) par Chirac", f3),
+    ("Afficher le(s) mot(s) le(s) plus répété(s) par Chirac", lambda: max_repeated("Chirac")),
     ("Afficher le(s) nom(s) du (des) président(s) qui a (ont) parlé de la « Nation » et le plus de fois", f4),
     ("Recherche le premier président qui a parlé de climat", f5),
     ("Recherche les mots évoqués par tout les présidents", f6)
