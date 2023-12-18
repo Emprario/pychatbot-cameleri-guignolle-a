d5b34041e811df51de927a5ba517e0837df9accb
Emprario <pierre.cameleri@free.fr>
2023-12-18 18:39:37 +0100

    Refonte totale du concept de menue, refonte de la partie 1

--- menu.py
+++ menu.py
@@ -1,25 +1,23 @@
+"""
+Projet : pyChatBot
+Auteurs : Pierre Cameleri, Raphaël Guignolle
+Description : Gestion de menus
+"""
+
 from functions import *
 from tf_idf import *
-from os import system, name
+from consts import *
+from os import listdir
 
 
-def clean() -> None:
+def less_important_words(verbose: bool, seuil: int = -1) -> list[str]:
     """
-    Efface la console (selon OS)
+    Renvoie les mots avec un score tf-idf inférieur ou égale à un seuil (zéro compris)
+    :param verbose: permet l'affichage ou non du résultat
+    :param seuil: indique le seuil minimum (optionnel)
     """
-    if name == "nt":
-        system("cls")
-    else:
-        system("clear")
-
-
-def less_important_words() -> None:
-    """
-    Affiche les mots avec un score tf-idf inférieur à un certain seuil
-    """
-    seuil = 0
-    while seuil <= 0:
-        seuil = float(input("Indiquez le score maximum souhaité : "))
+    while seuil < 0:
+        seuil = float(input("Indiquez le score maximum souhaité (0 pour les mots \"non-importants\") : "))
 
     matrice = tf_idf_matrice(CORPUS_CLEAN)
     tfidf_seuil = []
@@ -32,7 +30,9 @@ def less_important_words() -> None:
         if score <= seuil:
             tfidf_seuil.append(word)
 
-    print_list("Mots les moins importants (selon seuil) :", tfidf_seuil)  # Affichage
+    if verbose:
+        print_list("Mots les moins importants (selon seuil) :", tfidf_seuil)  # Affichage
+    return tfidf_seuil
 
 
 def max_tfidf_words() -> None:
@@ -55,11 +55,14 @@ def max_tfidf_words() -> None:
     print_list("Mot(s) ayant le score TF-IDF le plus élevé :", list_words)  # Affichage
 
 
-def max_repeated(president: str) -> None:
+def max_repeated() -> None:
     """
-    Affiche le(s) mot(s) le(s) plus répété(s) par le président fourni
-    :param president: le président à étudier
+    Affiche le(s) mot(s) le(s) plus répété(s) par le président fourni hormis mot non important
     """
+    lst_president = list(NOM_PRENOM.keys())
+    choice = choose_among("Choisir un président", lst_president)
+    president = lst_president[choice]
+
     infile = ""  # Création d'un grand texte à partir des discours du président
     for file in extract_names(CORPUS_CLEAN)[president]:
         target = open(CORPUS_CLEAN + file, 'r', encoding="utf-8")
@@ -67,94 +70,84 @@ def max_repeated(president: str) -> None:
         target.close()
     tf = tf_occurences(infile)  # Calcul d'occurrence des mots dans tous les discours du président
 
-    # Recherche du(des) mot(s) le plus occurrences
+    # Recherche du(des) mot(s) avec le plus occurrences
     occ_max = 0
     list_words = []
+    not_important = less_important_words(False, 0)
     for word in tf:
-        if tf[word] > occ_max:
+        if tf[word] > occ_max and word not in not_important:
             occ_max = tf[word]
             list_words = [word]
-        elif tf[word] == occ_max:
+        elif tf[word] == occ_max and word not in not_important:
             list_words.append(word)
 
-    print_list("Mot(s) le(s) plus répété(s) par le président Chirac :", list_words)  # Affichage
+    print_list("Mot(s) le(s) plus répété(s) par le président " + president + " :", list_words)  # Affichage
 
 
-def lookfor_word(word: str, most: bool) -> None:
+def lookfor_word(most: bool) -> None:
     """
     Affiche le nom du (des) président(s) qui ont parlé d'un certain mot
     Avec la possibilité d'afficher celui qui en a parlé le plus
-    :param word: le mot à rechercher
     :param most: active l'affichage de celui qui en parle le plus
     """
-    tfidf = tf_idf_matrice(CORPUS_CLEAN)
+
+    word = ""
+    while word == "":
+        word = input("Entrer un mot simple : ")
+    word = format_text(word)
+
+    tfmatrice = {}
+    for file in listdir(CORPUS_CLEAN):
+        with open(CORPUS_CLEAN + file, 'r', encoding='utf-8') as speech:
+            tfmatrice[file] = tf_occurences(speech.read())
 
     association = extract_names(CORPUS_CLEAN)  # Récupération d'un dictionnaire président : list[fichier]
 
     list_president = []
-    tfidf_max = 0
+    tf_max = 0
     president_max = ""
 
-    for file in tfidf[word]:
-        if tfidf[word][file] > 0:  # Signifie que le président en parle 
+    for file in tfmatrice:
+        if word in tfmatrice[file]:  # Signifie que le président en parle
             for president in association:
                 if file in association[president]:  # Récupération du nom du président qui a prononcé le discours
                     list_president.append(president)
-                    if tfidf[word][file] > tfidf_max:  # Définition de celui qui en parle le plus
-                        tfidf_max = tfidf[word][file]
+                    if tfmatrice[file][word] > tf_max:  # Définition de celui qui en parle le plus
+                        tf_max = tfmatrice[file][word]
                         president_max = president
 
     print_list(f"Président(s) qui a/ont parlés de '{word}' :", set(list_president))  # Affichage
     if most:
-        print("Le président qui en a le plus parlé est :", president_max)
+        print(f"Le président qui en a le plus parlé est {president_max} avec {tf_max} occurences")
 
 
-def common_words() -> None:
-    """
-    Affiche les mots qui sont dits par tous les présidents dans leurs investitures. NB : aucun mot n'est
-    non-important, car pour TF-IDF = 0 ⇔ Tous les IDF = 0 (parce que TF != 0 sinon pas dans la liste)
-    Or log(X) = 0 ⇒ X = 1 ⇒ nb_doc/nb_apparition = 0 ⇒ nb_doc = 0 Impossible donc TF-IDF != 0
-    Ainsi, on ne prend pas en compte si un mot est non-importants, car il est nécessairement important
-    """
-    matrice = tf_idf_matrice(CORPUS_CLEAN)
-    list_words = []
-    for word in matrice:
-        if 0 not in matrice[word].values():  # Si 0 appartient à un TF-IDF ⇒ TF = 0 ⇒ Le mot n'est pas dit par président
-            list_words.append(word)
-
-    print_list("Le(s) mot(s) évoqué(s) par tous les présidents sont : ", list_words)
-
-
-CORPUS_CLEAN = "./cleaned/"
-CORPUS_IN = "./speeches/"
-NB_TEXT = len(listdir(CORPUS_IN))
-OPTIONS = (
+OPTIONS_MENU = (
     ("Sortir", exit),
-    ("Afficher la liste des mots les moins importants", less_important_words),
+    ("Afficher la liste des mots les moins importants", lambda: less_important_words(True)),
     ("Afficher le(s) mot(s) ayant le score TF-IDF le plus élevé", max_tfidf_words),
-    ("Afficher le(s) mot(s) le(s) plus répété(s) par Chirac", lambda: max_repeated("Chirac")),
-    ("Afficher le(s) nom(s) du (des) président(s) qui a (ont) parlé de la « Nation » et le plus de fois", 
-     lambda: lookfor_word("nation", True)),
-    ("Recherche le premier président qui a parlé de climat", lambda: lookfor_word("climat", False)),
-    ("Recherche les mots évoqués par tout les présidents", common_words)
+    ("Afficher le(s) mot(s) le(s) plus répété(s) par un président", max_repeated),
+    ("Afficher le(s) nom(s) du (des) président(s) qui a (ont) parlé d'un mot et le plus de fois", lambda: lookfor_word(True)),
+    ("Recherche le(s) président(s) qui a/ont parlé d'un mot", lambda: lookfor_word(False)),
 )
 
-if __name__ == "__main__":
-    print("Prétraitement des données ...")
-    format_incorpus(CORPUS_IN, CORPUS_CLEAN)
 
+def do_menu(options: tuple[str, None], real_exit: bool) -> None:
+    """
+    Affiche un menu de sélection et gère son éxécution
+    :param options: tuple de combinaison description et action
+    :param real_exit: indique si la fonction doit réellement quitter le programme
+    """
     while True:
-        print("Que voulez-vous faire ?")
-
-        # Affiche les options disponibles
-        for i in range(len(OPTIONS)):
-            print(f"\t{i}. " + OPTIONS[i][0])
-
-        # Récupère la réponse et éxécute la bonne fonction
-        reply = input("> ")
-        for i in range(len(OPTIONS)):
-            if reply == str(i):
-                OPTIONS[i][1]()
+        choice = choose_among("Que voulez-vous faire ?", [option[0] for option in options])
+        if choice == 0 and not real_exit:
+            clean()
+            break
+        options[choice][1]()
 
         input("Passer à la suite ...")
         clean()
+
+
+if __name__ == "__main__":
+    print("Do not run this file.")
+    print("Run ./main.py instead")
